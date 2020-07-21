2745caa3b33d687b3b6734f784909526313a84d3
kohama66 <l.my.me666666@gmail.com>
2020-07-21 12:45:57 +0900

    add sqlboiler

---
+++ api/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nickname` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
