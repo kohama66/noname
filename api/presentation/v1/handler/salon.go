62bef074c5feba5f5204212bea9341f1b843410a
kohama66 <l.my.me666666@gmail.com>
2020-10-12 16:52:21 +0900

    add handler

---
+++ api/presentation/v1/handler/salon.go
@@ -0,0 +1,42 @@
+package handler
+
+import (
+	"net/http"
+)
+
+type salon struct{}
+
+// Salon DIInterface
+type Salon interface {
+	Find(w http.ResponseWriter, r *http.Request)
+}
+
+// NewSalon DI初期化関数
+func NewSalon() Salon {
+	return salon{}
+}
+
+// Find
+// @Summary 美容院検索
+// @Accept  json
+// @Produce  json
+// @Param data body requestmodel.SalonFind true "Request body"
+// @Success 200 {object} responsemodel.SalonFind
+// @Failure 500 {object} resource.Error "Something went wrong"
+// @Router /api/v1/salon/find [get]
+func (s salon) Find(w http.ResponseWriter, r *http.Request) {
+	// req, err := request.NewSalonFind(r)
+	// if err != nil {
+	// 	log.Warningf(r.Context(), "SalonFind.Request %v", err)
+	// 	factory.ErrorJSON(w, err.Error(), http.StatusBadRequest)
+	// 	return
+	// }
+	// res, err := b.salonUsecase.Find(r.Context(), req)
+	// if err != nil {
+	// 	log.Errorf(r.Context(), "SalonFind: %v", err)
+	// 	factory.ErrorJSON(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+	// factory.JSON(w, res)
+	// return
+}
