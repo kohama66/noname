3b48e04ce2986f3441a13871ef9d461d1a14b0a9
kohama66 <l.my.me666666@gmail.com>
2020-11-11 11:33:53 +0900

    add mypage デザイン作成

--- web/src/components/views/guest/mypage/Mypage.tsx
+++ web/src/components/views/guest/mypage/Mypage.tsx
@@ -1,31 +1,108 @@
 import React, { FC } from 'react';
+import Title from '../parts/Title/Title';
 import './Mypage.scss'
 
 const Mypage: FC = () => {
   return (
     <div id="guest-mypage">
-      <div className="profile-card">
-        <figure></figure>
-        <div>
-          <dl>
-            <span>
-              <dt>Name</dt>
-              <dd>小濱 陵</dd>
-            </span>
-            <span>
-              <dt>Age</dt>
-              <dd>29</dd>
-            </span>
-            <span>
-              <dt>Gender</dt>
-              <dd>man</dd>
-            </span>
-          </dl>
+      <Title title="MY PAGE" text="マイページ"/>
+      <section>
+        <div className="profile-card">
+          <figure></figure>
+          <div>
+            <dl>
+              <span>
+                <dt>名前</dt>
+                <dd>小濱 陵</dd>
+              </span>
+              <span>
+                <dt>歳</dt>
+                <dd>29</dd>
+              </span>
+              <span>
+                <dt>性別</dt>
+                <dd>man</dd>
+              </span>
+              <span>
+                <dt>電話</dt>
+                <dd>09012345678</dd>
+              </span>
+            </dl>
+          </div>
         </div>
-      </div>
-      <div className="reserved-history-card">
-
-      </div>
+        <div className="reserved-history-card">
+          <div>
+            <h2>現在の予約</h2>
+            <div>
+              <dl>
+                <span>
+                  <dt>店舗</dt>
+                  <dd>Kyoto Salon</dd>
+                </span>
+                <span>
+                  <dt>美容師</dt>
+                  <dd>山田 隆</dd>
+                </span>
+                <span>
+                  <dt>日付</dt>
+                  <dd>11月05日</dd>
+                </span>
+                <span>
+                  <dt>時間</dt>
+                  <dd>15:00から</dd>
+                </span>
+                <span className="menus">
+                  <dt>メニュー</dt>
+                  <span>
+                    <dd>カット</dd>
+                    <dd>カラー</dd>
+                    <dd>パーマ</dd>
+                  </span>
+                </span>
+                <span>
+                  <dt>合計</dt>
+                  <dd>5500</dd>
+                </span>
+              </dl>
+            </div>
+          </div>
+          <div>
+            <h2>前回の予約</h2>
+            <div>
+              <dl>
+                <span>
+                  <dt>店舗</dt>
+                  <dd>Kyoto Salon</dd>
+                </span>
+                <span>
+                  <dt>美容師</dt>
+                  <dd>山田 隆</dd>
+                </span>
+                <span>
+                  <dt>日付</dt>
+                  <dd>11月05日</dd>
+                </span>
+                <span>
+                  <dt>時間</dt>
+                  <dd>15:00から</dd>
+                </span>
+                <span className="menus">
+                  <dt>メニュー</dt>
+                  <span>
+                    <dd>カット</dd>
+                    <dd>カラー</dd>
+                    <dd>パーマ</dd>
+                  </span>
+                </span>
+                <span>
+                  <dt>合計</dt>
+                  <dd>5500</dd>
+                </span>
+              </dl>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
