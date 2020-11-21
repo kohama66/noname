2199aafae68494d3d2999e1f5324823fdccb3b62
kohama66 <l.my.me666666@gmail.com>
2020-11-21 14:47:55 +0900

    add disabled

--- web/src/components/views/login/Login.tsx
+++ web/src/components/views/login/Login.tsx
@@ -1,16 +1,26 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import Input from '../parts/formParts/Input';
 import './Login.scss'
 
 const Login: FC = () => {
+  const [email, setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  const [disabled, setDisabled] = useState<boolean>(false)
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setDisabled(true)
+  }
+
   return (
     <div id="login">
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>メールアドレス</label>
-        <Input type="email" value=""/>
+        <Input type="email" value={email} setState={setEmail} required={true} maxLength={50} disabled={disabled}/>
         <label>パスワード</label>
-        <Input type="password" value="" />
-        <Input type="submit" value="ログイン"/>
+        <Input type="password" value={password} setState={setPassword} required={true} minLength={8}
+          placeHolder="8文字以上で入力してください" disabled={disabled}/>
+        <Input type="submit" value="ログイン" disabled={disabled}/>
       </form>
     </div>
   )
--- web/src/components/views/parts/formParts/Input.tsx
+++ web/src/components/views/parts/formParts/Input.tsx
@@ -1,14 +1,31 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import './Input.scss'
 
 interface props {
   type: "text" | "email" | "password" | "submit"
   value: string
+  setState?: React.Dispatch<React.SetStateAction<string>>
+  required?: true
+  maxLength?: number
+  minLength?: number
+  placeHolder?: string
+  disabled?: boolean
 }
 
 const Input: FC<props> = (props) => {
+  // const [disabled, setDisabled] = useState<boolean>(false)
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    if (props.setState) {
+      props.setState(e.currentTarget.value)
+    }
+  }
+
   return (
-    <input type={props.type} value={props.value} />
+    <input type={props.type} value={props.value} onChange={handleChange} required={props.required}
+      maxLength={props.maxLength} minLength={props.minLength} placeholder={props.placeHolder}
+      disabled={props.disabled}
+    />
   )
 }
 
