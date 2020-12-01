fb1379887bb09044aa8dbd1fd0b2aa2ef943158b
kohama66 <l.my.me666666@gmail.com>
2020-12-01 19:15:33 +0900

    add 日付レスポンシブ

--- web/src/components/views/parts/Schedule/index.tsx
+++ web/src/components/views/parts/Schedule/index.tsx
@@ -27,4 +27,25 @@ const Schedule: FC<props> = (props) => {
   )
 };
 
+export const Schedule_smartphoneResponsive: FC<props> = (props) => {
+  const [oneWeeks, setOneWeeks] = useState<Date[]>([])
+
+  const getWeek = () => {
+    const weeks = []
+    for (let i = 0; i < 7; i++) {
+      weeks.push(new Date())
+      weeks[i].setDate(weeks[i].getDate() + i)
+    }
+    setOneWeeks(weeks)
+  }
+
+  useEffect(() => {
+    getWeek()
+  }, [])
+
+  return (
+    <ScheduleComponent weeks={oneWeeks} reservations={props.reservations} />
+  )
+};
+
 export default Schedule
